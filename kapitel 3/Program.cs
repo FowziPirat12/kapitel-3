42bcd4ae7c120d81a50706269b07315836a9c9b4
Admin <Admin@TEB127029>
2022-09-23 11:16:41 +0200

    Add project files.

---
+++ kapitel 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Upggift_3._13
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16)
+            {
+                Console.WriteLine("Du kan delta i tävlingen");
+            }
+            else if (ålder >= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else if (ålder >= 19)
+            {
+                Console.WriteLine("Du är för gammal du kan inte delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Tyvär, du är för ung. Du kan inte delta i tävlingen");
+            }
+        }
+    }
+}
